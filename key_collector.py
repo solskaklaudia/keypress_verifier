04dc96f7319aab7fe1b18da05b8cccc234798751
solskaklaudia <solska.klaudia@gmail.com>
2021-10-29 17:22:24 +0200

    collect keys info in a list

--- key_collector.py
+++ key_collector.py
@@ -1,18 +1,46 @@
 from pynput.keyboard import Key, Listener
 import time
 
+keys = []
+
 def on_press(key):
     elapsed_time = time.perf_counter()
     print('{0} pressed at {1}'.format(
         key, elapsed_time))
 
+    key = {
+            "key": key,
+            "status": "pressed",
+            "time": elapsed_time
+        }
+
+    if(len(keys) > 0):
+        # Add key press entry to list if it's not already added,
+        # prevents adding multiple entries of the same key when holding
+        if(not(keys[-1]["key"] == key["key"] and keys[-1]["status"] == "pressed")):
+            keys.append(key)
+
+    else:
+        keys.append(key)
+
+
 def on_release(key):
     elapsed_time = time.perf_counter()
     print('{0} released at {1}'.format(
         key, elapsed_time))
-    if key == Key.esc:
+
+    key = {
+        "key": key,
+        "status": "released",
+        "time": elapsed_time
+    }
+
+    keys.append(key)
+
+    if key["key"] == Key.esc:
         # Stop listener
         return False
+
 
 # Collect events until released
 with Listener(
